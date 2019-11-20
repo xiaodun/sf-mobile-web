2d73157ead6b3249eac2cc6882c55334c527b687
xiaodun <1947147481@qq.com>
2019-11-20 21:00:08 +0800

    手脑通-增加上一个、下一个按钮
    便于快速切换视频

--- src/components/tools/player.vue
+++ src/components/tools/player.vue
@@ -32,7 +32,9 @@
 
       display: block;
 
-      height: 150px;
+      #video-id {
+        height: 150px;
+      }
     }
 
     .list-wrapper {
@@ -41,10 +43,36 @@
   }
 
   .video-wrapper {
-    height: 250px;
+    background: #fff;
+
     padding-top: 10px;
 
     background-color: #fff;
+
+    //上一个、下一个
+    .prevbtn,
+    .nextbtn {
+      width: 49%;
+      height: 30px;
+    }
+    .prevbtn {
+      float: left;
+    }
+    .nextbtn {
+      float: right;
+    }
+    &:after {
+      clear: both;
+      content: " ";
+      display: block;
+    }
+
+    #video-id {
+      width: 100%;
+      height: 240px;
+
+      background: black;
+    }
   }
 
   .list-wrapper {
@@ -60,27 +88,13 @@
       }
     }
   }
-
-  #video-id {
-    width: 100%;
-    height: 100%;
-
-    background: black;
-  }
 }
-
 </style>
 
 <template>
-  <div
-    id="player-vue-id"
-    ref="parentDom"
-  >
+  <div id="player-vue-id" ref="parentDom">
     <mt-tab-container v-model="model">
-      <mt-tab-container-item
-        class="player-tab"
-        id="player-tab-id"
-      >
+      <mt-tab-container-item class="player-tab" id="player-tab-id">
         <div class="video-wrapper">
           <video
             autoplay
@@ -89,29 +103,53 @@
             controls
             :src="active.src"
           ></video>
+          <mt-button @click="on_change_video(-1)" class="prevbtn"
+            >上一个</mt-button
+          >
+          <mt-button @click="on_change_video(1)" class="nextbtn"
+            >下一个</mt-button
+          >
         </div>
-        <div
-          class="list-wrapper"
-          v-if="moveList && moveList.length>0"
-        >
+        <div class="list-wrapper" v-if="moveList && moveList.length > 0">
           <mt-cell-swipe
-            :left="[item.isTemp ? []:{handler:()=>request_download_file(item),content:'下载',style:{backgroundColor:'#74fd3d',color:'#fff',handle:()=>down_file(item)}}]"
-            :right="item.isUser || item.isTemp ?[{handler:()=>request_delete_file(item,index),content:'删除',style:{background:'#fd593d',color:'#fff'}}]:[]"
+            :left="[
+              item.isTemp
+                ? []
+                : {
+                    handler: () => request_download_file(item),
+                    content: '下载',
+                    style: {
+                      backgroundColor: '#74fd3d',
+                      color: '#fff',
+                      handle: () => down_file(item),
+                    },
+                  },
+            ]"
+            :right="
+              item.isUser || item.isTemp
+                ? [
+                    {
+                      handler: () => request_delete_file(item, index),
+                      content: '删除',
+                      style: { background: '#fd593d', color: '#fff' },
+                    },
+                  ]
+                : []
+            "
             :title="item.name"
-            :label="!item.isUser ? item.isTemp ?'本地文件': '系统'  :'用户'"
+            :label="!item.isUser ? (item.isTemp ? '本地文件' : '系统') : '用户'"
             @click.native="player(item)"
             :class="active.id === item.id ? 'active' : ''"
             class="move"
             :key="Math.random()"
-            v-for="(item,index) in  moveList"
+            v-for="(item, index) in moveList"
           >
             <!-- 解决添加数据后 显示错乱的问题   :key="Math.random()"   -->
           </mt-cell-swipe>
         </div>
-        <div
-          class="no-data"
-          v-if="moveList && moveList.length === 0"
-        >暂无视频</div>
+        <div class="no-data" v-if="moveList && moveList.length === 0">
+          暂无视频
+        </div>
       </mt-tab-container-item>
       <mt-tab-container-item id="config-tab-id">
         <localize-card>
@@ -121,7 +159,9 @@
             size="16px"
           ></LocalizeIconfont>
           <div slot="title">电脑目录</div>
-          <div slot="content">程序从电脑的 C:\sf-mobile-web\player\system\movie 获取视频</div>
+          <div slot="content">
+            程序从电脑的 C:\sf-mobile-web\player\system\movie 获取视频
+          </div>
         </localize-card>
         <localize-card>
           <LocalizeIconfont
@@ -134,7 +174,7 @@
             <div class="item">存储至:C:\sf-mobile-web\player\user\movie</div>
             <LocalizeUpload
               @success="upload_success"
-              :uri="requestPlayerPrefix+'/upload'"
+              :uri="requestPlayerPrefix + '/upload'"
               multiple
               class="item"
               accept="video/*"
@@ -165,17 +205,16 @@
           ></LocalizeIconfont>
           <div slot="title">关于程序</div>
           <div slot="content">
-            <p>为了减缓手机内存的压力,自定义的本地视频播放体验,应个人需求而制作。</p>
+            <p>
+              为了减缓手机内存的压力,自定义的本地视频播放体验,应个人需求而制作。
+            </p>
             <p>碍于浏览器支持的音频格式有限,无法播放视频实属正常!</p>
           </div>
         </localize-card>
       </mt-tab-container-item>
     </mt-tab-container>
 
-    <mt-tabbar
-      fixed
-      v-model="model"
-    >
+    <mt-tabbar fixed v-model="model">
       <mt-tab-item id="player-tab-id">播放器</mt-tab-item>
       <mt-tab-item id="config-tab-id">配置</mt-tab-item>
     </mt-tabbar>
@@ -192,18 +231,37 @@ export default {
       requestPlayerPrefix: "/player/player",
       active: {
         src: "",
-        id: ""
+        id: "",
       },
-      moveList: null
+      moveList: null,
     };
   },
   methods: {
+    on_change_video(argStep) {
+      //切换视频
+      //当前视频再列表的位置的位置
+      const id = this.active.id;
+      let index = this.moveList.findIndex((item) => {
+        if (item.id === id) {
+          return true;
+        }
+      });
+
+      index = (index || 0) + argStep;
+      if (index < 0) {
+        index = this.moveList.length - 1;
+      }
+      if (index > this.moveList.length - 1) {
+        index = 0;
+      }
+      this.player(this.moveList[index]);
+    },
     readerSuccess(argFile) {
       this.moveList.unshift({
         name: argFile.name,
         isTemp: true,
         id: ((Math.random() * 100000) | 0) + argFile.name,
-        src: URL.createObjectURL(argFile)
+        src: URL.createObjectURL(argFile),
       });
     },
     request_delete_file(argItem, index) {
@@ -215,10 +273,10 @@ export default {
             method: "post",
             url: this.requestPlayerPrefix + "/delete",
             data: {
-              id: argItem.id
-            }
+              id: argItem.id,
+            },
           })
-          .then(response => {
+          .then((response) => {
             Toast("删除成功");
 
             this.request_player_get();
@@ -237,11 +295,11 @@ export default {
           url: this.requestPlayerPrefix + "/download",
           data: {
             id: argItem.isUser ? argItem.id : argItem.name,
-            isUser: argItem.isUser
+            isUser: argItem.isUser,
           },
-          responseType: "blob"
+          responseType: "blob",
         })
-        .then(response => {
+        .then((response) => {
           var blob = response.data;
           var a = document.createElement("a");
           a.download = argItem.name;
@@ -258,9 +316,9 @@ export default {
         .request({
           method: "post",
           url: this.requestPlayerPrefix + "/get",
-          data: {}
+          data: {},
         })
-        .then(response => {
+        .then((response) => {
           this.$refs.videoDom.src = undefined;
           this.moveList = response.data;
         });
@@ -311,7 +369,7 @@ export default {
     player(argMove) {
       this.active.id = argMove.id;
       this.request_player_play(argMove);
-    }
+    },
   },
   computed: {},
   mounted() {
@@ -328,11 +386,11 @@ export default {
     model() {
       document.documentElement.scrollTop = 0;
       document.body.scrollTop = 0;
-    }
+    },
   },
   beforeDestory() {
     window.removeEventListener("scroll", this.onScroll);
     document.removeEventListener("touchend", this.onChangeDropmenu);
-  }
+  },
 };
 </script>
